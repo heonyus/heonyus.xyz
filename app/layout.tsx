07dabe285f1c10accd0bc780ca8afe89b9d6bbda
honey <108461006+sparkerhoney@users.noreply.github.com>
2024-08-05 21:26:19 +0900

    Fix: domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "jay.ai",
+    template: "%s | jay.ai",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "jay.ai",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://jay.ai",
+    siteName: "jay.ai",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://jay.ai/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "jay",
     card: "summary_large_image",
   },
   icons: {
